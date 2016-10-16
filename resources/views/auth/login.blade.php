94e6655bdc4e7d06c2db6e3bfa05bbef0b51d58e
Rogerio Azevedo <rog_reg@hotmail.com>
2016-10-16 03:37:51 -0200

    Add Vendor

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,7 +42,7 @@
                             <div class="col-md-6 col-md-offset-4">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember"> Remember Me
+                                        <input type="checkbox" name="remember"> Lembrar-me
                                     </label>
                                 </div>
                             </div>
@@ -55,7 +55,7 @@
                                 </button>
 
                                 <a class="btn btn-link" href="{{ url('/password/reset') }}">
-                                    Forgot Your Password?
+                                    Esqueceu sua senha?
                                 </a>
                             </div>
                         </div>
